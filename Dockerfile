0e4fa7732baeab4d49d659a02b2e375fe458d39e
Dev khadka <devbahadurkhadka@SMS-EXE-Dev.local>
2017-09-05 22:43:42 +0545

    changed parent docker file to  not user entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,7 @@
-FROM devkhadka/ubuntuwithssh:1.0.1
+FROM devkhadka/ubuntuwithssh:1.0.2
+
+##If you need to run some command when docker is started un-comment following three lines 
+##and add your scripts in entrypoint.sh file in root of this project
+#ADD entrypoint.sh /root/entrypoint.sh
+#RUN chmod 777 /root/entrypoint.sh
+#ENTRYPOINT /root/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
-# Don't remove this command is needed to start ssh server when docker is started
-# This command start ssh server in forground server below you can remove -D option
-/usr/sbin/sshd -D
+# Don't remove this command, it is needed to start ssh server when docker is started
+/usr/sbin/sshd
 
-# You can add your other entry point commands here like starting your http server etc.
+# You can add your other commands here like starting your http server etc. 
+#which will be called when container is started
+
+# You need to run a long running service at the end of this file so that docker contain is not
+# terminated immediately, like starting your HTTP server in forground 
+# or un-comment following command
+#touch root/temp.log && tail -f root/temp.log
